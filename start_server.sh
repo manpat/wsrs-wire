b306dc9987c59e3e3425a9fa527eec285551a1a6
manpat <pat@patsown.com>
2017-09-11 04:20:00 +0100

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -2,10 +2,10 @@
 
 cd server > /dev/null
 
-for session in $(screen -ls | grep -o '[0-9]*\.server'); do
+for session in $(screen -ls | grep -o '[0-9]*\.wire_server'); do
 	screen -S "${session}" -X quit;
 done
 
-screen -dmS "server" bash -i
+screen -dmS "wire_server" bash -i
 sleep 0.1 # shutup
-screen -r -S "server" -p 0 -X stuff $'RUST_BACKTRACE=1 cargo run\r'
+screen -r -S "wire_server" -p 0 -X stuff $'RUST_BACKTRACE=1 cargo run\r'
